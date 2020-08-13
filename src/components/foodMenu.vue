db01713cc963974e5866e26ee50e4e341177c4e6
Ibrohim <ibrohim@pop-os.localdomain>
2020-08-13 22:03:10 +0500

    foodMenu works

--- src/components/foodMenu.vue
+++ src/components/foodMenu.vue
@@ -9,7 +9,7 @@
         <img src="images/5ded2dde5b588.png" alt="shurma" />
       </div>
       <div class="container">
-        <h2>Food Menu</h2>
+        <h2 @click="foodMenu">Food Menu</h2>
 
         <div class="main_menu_nav">
           <div class="row justify-content-center">
@@ -32,6 +32,49 @@
         <div class="main_menu_main">
           <div class="row justify-content-around">
             <div class="col-12 col-md-6 col-lg-5">
+              
+              <div 
+                v-for="(div, index) in foodMenu()" :key="index">
+                  <div class="main_menu_block">
+                <div class="block_text">
+                  <h4>{{ div.name }}</h4>
+                  <p>{{ div.description }}</p>
+                </div>
+                <span><i class="fa fa-dollar"></i>{{ div.price }}</span>
+              </div>
+              </div>
+              
+              <!-- <div class="main_menu_block">
+                <div class="block_text">
+                  <h4>Ultimate organic fruit salad</h4>
+                  <p>survived not only five centuries but the leap</p>
+                </div>
+                <span><i class="fa fa-dollar"></i>50.00</span>
+              </div>
+              <div class="main_menu_block">
+                <div class="block_text">
+                  <h4>Plain pancakes</h4>
+                  <p>Donec eget augue at diam euismod viverra</p>
+                </div>
+                <span><i class="fa fa-dollar"></i>50.00</span>
+              </div>
+              <div class="main_menu_block">
+                <div class="block_text">
+                  <h4>Toasted jam</h4>
+                  <p>Phasellus a ex accumsan, sollicitudin</p>
+                </div>
+                <span><i class="fa fa-dollar"></i>50.00</span>
+              </div>
+              <div class="main_menu_block">
+                <div class="block_text">
+                  <h4>Toasted jam</h4>
+                  <p>Phasellus a ex accumsan, sollicitudin</p>
+                </div>
+                <span><i class="fa fa-dollar"></i>50.00</span>
+              </div> -->
+            </div>
+
+            <!-- <div class="col-12 col-md-6 col-lg-5">
               <div class="main_menu_block">
                 <div class="block_text">
                   <h4>Ultimate organic fruit salad</h4>
@@ -60,37 +103,7 @@
                 </div>
                 <span><i class="fa fa-dollar"></i>50.00</span>
               </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-5">
-              <div class="main_menu_block">
-                <div class="block_text">
-                  <h4>Ultimate organic fruit salad</h4>
-                  <p>survived not only five centuries but the leap</p>
-                </div>
-                <span><i class="fa fa-dollar"></i>50.00</span>
-              </div>
-              <div class="main_menu_block">
-                <div class="block_text">
-                  <h4>Plain pancakes</h4>
-                  <p>Donec eget augue at diam euismod viverra</p>
-                </div>
-                <span><i class="fa fa-dollar"></i>50.00</span>
-              </div>
-              <div class="main_menu_block">
-                <div class="block_text">
-                  <h4>Toasted jam</h4>
-                  <p>Phasellus a ex accumsan, sollicitudin</p>
-                </div>
-                <span><i class="fa fa-dollar"></i>50.00</span>
-              </div>
-              <div class="main_menu_block">
-                <div class="block_text">
-                  <h4>Toasted jam</h4>
-                  <p>Phasellus a ex accumsan, sollicitudin</p>
-                </div>
-                <span><i class="fa fa-dollar"></i>50.00</span>
-              </div>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
@@ -102,12 +115,8 @@
 export default {
   data() {
     return {
+        curentMenu: "breakfast",
       foodTitles: [
-        {
-          title: "All",
-          icon: "fa fa-book-open",
-          exact: true,
-        },
         {
           title: "Breakfast",
           icon: "fa fa-mug-hot",
@@ -138,7 +147,154 @@ export default {
           icon: "fas fa-bread-slice",
           exact: false,
         },
+        {
+          title: "All",
+          icon: "fa fa-book-open",
+          exact: true,
+        }
       ],
+      breakfast: [
+          {
+              name: "Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          },
+          {
+              name: "Plain pancakes",
+              description: "Donec eget augue at diam euismod viverra",
+              price: 50 
+          },
+          {
+              name: "Toasted jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          }
+          ,
+          {
+              name: "Toasted jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          },
+          {
+              name: "Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          }
+      ],
+      lunch: [
+          {
+              name: "Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 20 
+          },
+          {
+              name: "Toasted jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 20 
+          },
+          {
+              name: "Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 20 
+          }
+      ],
+      snacks: [
+          {
+              name: "organic fruit salad",
+              description: "only five centuries but the leap",
+              price: 10 
+          },
+          {
+              name: "pancakes",
+              description: "augue at diam euismod viverra",
+              price: 70 
+          },
+          {
+              name: "Jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 70 
+          }
+      ],
+      dinner: [
+          {
+              name: "Fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 40 
+          },
+          {
+              name: "Plain",
+              description: "Donec eget augue at diam euismod viverra",
+              price: 50 
+          },
+          {
+              name: "Toasted",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          }
+          ,
+          {
+              name: "Toasted jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          },
+          {
+              name: "Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          }
+      ],
+      pizza: [
+          {
+              name: "pizza fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          },
+          {
+              name: "Plain Pizza",
+              description: "Donec eget augue at diam euismod viverra",
+              price: 50 
+          },
+          {
+              name: "Toasted Pizza",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          }
+          ,
+          {
+              name: "Pizza jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          },
+          {
+              name: "Pizza Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          }
+      ],
+      soups: [
+          {
+              name: "Ultimate organic fruit",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          },
+          {
+              name: "Toasted soup",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          }
+          ,
+          {
+              name: "soup jam",
+              description: "Phasellus a ex accumsan, sollicitudin",
+              price: 50 
+          },
+          {
+              name: "Ultimate organic fruit salad",
+              description: "survived not only five centuries but the leap",
+              price: 50 
+          }
+      ],
+      all: []
     };
   },
   methods: {
@@ -147,7 +303,21 @@ export default {
         item.exact = false;
       }
       val.exact = true;
+      this.curentMenu = val.title.toLowerCase()
     },
+    foodMenu() {
+        let food = []
+        for(let i of this[this.curentMenu]) {
+            food.push(i)
+        }
+        return food
+    },
+    allFood() {
+        this.all = this.breakfast.concat(this.lunch, this.snacks, this.dinner, this.pizza, this.soups)    
+    } 
   },
+  created() {
+      this.allFood()
+  }
 };
 </script>
